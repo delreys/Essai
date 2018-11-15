9e600248d335e48264c4a42946b1d89825e1f3cc
delreys <45082502+delreys@users.noreply.github.com>
2018-11-15 23:59:34 +0100

    added E2

---
+++ MonTravail/HelloWordcmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstProgramWithCommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
